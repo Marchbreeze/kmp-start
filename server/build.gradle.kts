3efc7e413af37a212d7f6c69b4e77c6aec1ee93c
Sangho Kim <marchbreeze03@gmail.com>
2026-03-19 23:07:44 +0900

    feat: add jvm() target to shared module for server DTO sharing

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -9,6 +9,7 @@ application {
 }
 
 dependencies {
+    implementation(projects.shared)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.content.negotiation)
